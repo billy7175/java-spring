7867daf8647254d31d0dd1d5ba11d7e0d5c0a67f
billy7175 <68532934+billy7175@users.noreply.github.com>
2024-03-03 04:11:25 +0900

    initial commit

---
+++ src/test/java/com/example/demoartiface/DemoNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoartiface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
